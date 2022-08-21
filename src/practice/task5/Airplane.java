63ff3e2b284aaaf56cde82988b889e98c5e01afe
MSP1981 <99791179+MSP1981@users.noreply.github.com>
2022-08-21 11:15:14 +0300

    commit

---
+++ src/practice/task5/Airplane.java
@@ -0,0 +1,30 @@
+package practice.task5;
+
+public abstract class Airplane {
+    public String type;
+    public String model;
+    public int ecoPlaces;
+    public int busPlaces;
+
+    public Airplane(String type, String model) {
+        this.type = type;
+        this.model = model;
+    }
+
+    public Airplane(String type, String model, int ecoPlaces) {
+        this.type = type;
+        this.model = model;
+        this.ecoPlaces = ecoPlaces;
+    }
+
+    public Airplane(String type, String model, int ecoPlaces, int busPlaces) {
+        this.type = type;
+        this.model = model;
+        this.ecoPlaces = ecoPlaces;
+        this.busPlaces = busPlaces;
+    }
+
+    public void printAirplaneInfo() {
+    }
+}
+
